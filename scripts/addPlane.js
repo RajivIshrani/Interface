8c2879a513a1e83173c3bad0d3b883c81832d96e
Ishrani Rajiv <rajivishrani@gmail.com>
2022-10-19 14:07:30 +0530

    initial commit

---
+++ scripts/addPlane.js
@@ -0,0 +1,2 @@
+// 1.deploy plane registry and interactor contract
+// 2.call the add plane function using interactor contract 
---
+++ scripts/removePlane.js
@@ -0,0 +1,3 @@
+// 1.deploy plane registry and interactor contract
+// 2.call the remove plane function using interactor contract
+// npx hardhat run ./scripts/ 
