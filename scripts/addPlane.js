eeab73cf28a64295b977694784d6473af745a99e
Ishrani Rajiv <rajivishrani@gmail.com>
2022-10-19 15:23:35 +0530

    initial commit

--- scripts/addPlane.js
+++ scripts/addPlane.js
@@ -1,2 +1,38 @@
-// 1.deploy plane registry and interactor contract
-// 2.call the add plane function using interactor contract 
+// 1.deploy PlaneRegistry and PlaneInteractor contract
+// 2.call the addPlane function using PlaneInteractor contract
+
+const hre = require("hardhat")
+
+async function main() {
+    // deploy PlaneRegistry Contract
+    let planeInteractorAddress
+    const planeRegistry = await hre.ethers.getContractFactory("PlaneRegistry")
+    const planeRegistryInstance = await planeRegistry.deploy()
+    await planeRegistryInstance.deployed()
+    planeRegistryInstanceAddress = planeRegistryInstance.address
+    console.log("PlaneRegistry Address:", planeRegistryInstanceAddress)
+
+    // deploy PlaneInteractor Contract
+    let planeInteractorInstanceAddress
+    const planeInteractor = await hre.ethers.getContractFactory(
+        "PlaneInteractor"
+    )
+    const planeInteractorInstance = await planeInteractor.deploy()
+    await planeInteractorInstance.deployed()
+    planeInteractorInstanceAddress = planeInteractorInstance.address
+    console.log("PlaneInteractor Address:", planeInteractorInstanceAddress)
+
+    // Call addPlanes Function using PlaneInteractor Contract
+    const address = planeRegistryInstanceAddress
+    console.log("----------- ADDING PLANE NAMES -----------")
+
+    await planeInteractorInstance.calladdPlanes(address, "Air India")
+    await planeInteractorInstance.calladdPlanes(address, "Etihad")
+    await planeInteractorInstance.calladdPlanes(address, "Vistara")
+}
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error)
+        process.exit(1)
+    })
--- scripts/removePlane.js
+++ scripts/removePlane.js
@@ -1,3 +1,42 @@
-// 1.deploy plane registry and interactor contract
-// 2.call the remove plane function using interactor contract
-// npx hardhat run ./scripts/ 
+// 1.deploy PlaneRegistry and PlaneInteractor contract
+// 2.call the removePlane function using PlaneInteractor contract
+
+const hre = require("hardhat")
+
+async function main() {
+    // deploy PlaneRegistry Contract
+    let planeInteractorAddress
+    const planeRegistry = await hre.ethers.getContractFactory("PlaneRegistry")
+    const planeRegistryInstance = await planeRegistry.deploy()
+    await planeRegistryInstance.deployed()
+    planeRegistryInstanceAddress = planeRegistryInstance.address
+    console.log("PlaneRegistry Address:", planeRegistryInstanceAddress)
+
+    // deploy PlaneInteractor Contract
+    let planeInteractorInstanceAddress
+    const planeInteractor = await hre.ethers.getContractFactory(
+        "PlaneInteractor"
+    )
+    const planeInteractorInstance = await planeInteractor.deploy()
+    await planeInteractorInstance.deployed()
+    planeInteractorInstanceAddress = planeInteractorInstance.address
+    console.log("PlaneInteractor Address:", planeInteractorInstanceAddress)
+
+    // Call remvoePlanes Function using PlaneInteractor Contract
+    const address = planeRegistryInstanceAddress
+    console.log("----------- ADDING PLANE NAMES -----------")
+
+    await planeInteractorInstance.calladdPlanes(address, "Air India")
+    await planeInteractorInstance.calladdPlanes(address, "Etihad")
+    await planeInteractorInstance.calladdPlanes(address, "Vistara")
+
+    console.log("----------- REMOVING PLANE NAME -----------")
+    const index = 4
+    await planeInteractorInstance.calladdPlanes(address, index)
+}
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error)
+        process.exit(1)
+    })
