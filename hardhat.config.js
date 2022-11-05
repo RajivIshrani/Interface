8942bf757ed694d0684139370b6a17678ac466bf
Ishrani Rajiv <rajivishrani@gmail.com>
2022-11-05 17:34:45 +0530

    updated: added mainnet in network

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@ require("solidity-coverage")
 require("hardhat-deploy")
 require("dotenv").config()
 
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY
@@ -12,6 +13,13 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY
 module.exports = {
     solidity: "0.8.17",
     networks: {
+        mainnet: {
+            url: MAINNET_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 1,
+            blockConfirmations: 6
+        },
+
         goerli: {
             url: GOERLI_RPC_URL,
             accounts: [PRIVATE_KEY],
